b4f872a88e263582c0c5ccc4d5dcb06570cb7455
Devin Ho <djxho36@gmail.com>
2022-04-13 10:01:27 -0400

    commit tree 81744d29a40359c5b0077baf6fcc849658ccf4ad

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching (drivers, element) {
+    let match = drivers.filter(item => item.toLowerCase() === element.toLowerCase())
+    return match;
+}
+
+function fuzzyMatch(drivers, element){
+    let answer = drivers.filter((match) => match.substring(0,2) === element.substring(0,2))
+    return answer; 
+}
+
+function matchName(drivers, element) { 
+    let hometown = drivers.filter(item => item.name === element)
+    return hometown;
+
+}
